5dd96f1a254ee7cad1f5cbe2ae2550e342c62b92
dudaPetracek <157414482+dudaPetracek@users.noreply.github.com>
2025-03-18 14:39:59 -0300

    Add files via upload

---
+++ src/components/Formulario/index.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+const Formulario = ({ heigthValue, weightValue, calcImc }) => {
+    const handleKeyDown = (e) => {
+      // Impede a digitação do ponto (.), vírgula (,) e sinal de negativo (-)
+      if (e.key === "-" || e.key === "+") {
+        e.preventDefault();
+      }
+    };
+    return (
+      <>
+        <form className="mb-3">
+          <div className="form-group">
+            <label htmlFor="height" className="form-label">
+              Altura (cm):
+            </label>
+            <input
+              className="form-control"
+              id="height"
+              type="number"
+              placeholder="Informe a sua altura. Ex: 165"
+              onChange={heigthValue}
+              onKeyDown={handleKeyDown}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="weight" className="form-label">
+              Peso (kg):
+            </label>
+            <input
+              className="form-control"
+              id="weight"
+              type="number"
+              placeholder="Informe o seu peso. Ex: 62,5"
+              onChange={weightValue}
+              onKeyDown={handleKeyDown}
+            />
+          </div>
+          <button className="btn btn-dark mt-3" type="button" onClick={calcImc}>
+            Calcular
+          </button>
+        </form>
+      </>
+    );
+  };
+  
+  export default Formulario;
